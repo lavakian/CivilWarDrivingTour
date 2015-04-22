4dc3370e16c78701deffd3766eaec89fb3a281b7
Ryan Kiel <ryan@Ryans-MacBook-Pro.local>
2015-04-22 15:33:35 -0400

    Added some of the UI for the Comment stuff.

---
+++ CivilWarDrivingTour/SweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SweetTableViewCell.swift
+//  CivilWarDrivingTour
+//
+//  Created by Ryan Kiel on 4/22/15.
+//  Copyright (c) 2015 KoolKatz. All rights reserved.
+//
+
+import UIKit
+
+class SweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
